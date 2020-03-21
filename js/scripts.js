f599c818d0ec4cca996681a13c6e3a2b342316a1
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-03-21 08:39:55 +0600

    assignment 04

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+   $('#reservebutton').click(function(){
+
+   	$('#reserveModal').modal('show');
+   });
+
+   $('#loginlink').click(function(){
+
+   	$('#loginModal').modal('show');
+   });
+
+
+});
